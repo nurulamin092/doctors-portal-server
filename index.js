e1fd8f6a3d3ecd6d00390608b7178468cad53a4b
nurulamin092 <mnurulamin092@gmial.com>
2022-05-18 16:27:56 +0600

    Limit Dashboard features based on admin level access

--- index.js
+++ index.js
@@ -39,21 +39,34 @@ async function run() {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
-            res.send(services)
-
+            res.send(services);
         });
         app.get('/user', async (req, res) => {
             const users = await userCollection.find().toArray();
             res.send(users);
         });
-        app.put('/user/admin/:email', async (req, res) => {
+        app.get('/admin/:email', async (req, res) => {
             const email = req.params.email;
-            const filter = { email: email };
-            const updateDoc = {
-                $set: { role: 'admin' },
-            };
-            const result = await userCollection.updateOne(filter, updateDoc);
-            res.send(result)
+            const user = await userCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin })
+        })
+        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+                const filter = { email: email };
+                const updateDoc = {
+                    $set: { role: 'admin' },
+                };
+                const result = await userCollection.updateOne(filter, updateDoc);
+                res.send(result)
+            }
+            else {
+                res.status(403).send({ message: 'forbidden' });
+            }
+
         });
 
         app.put('/user/:email', async (req, res) => {
