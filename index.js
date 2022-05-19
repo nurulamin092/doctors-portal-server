b403cc4c22f6975ad3f3e8973f5151df8c564094
nurulamin092 <mnurulamin092@gmial.com>
2022-05-19 12:45:00 +0600

    doctor post api create

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ async function run() {
         const serviceCollection = client.db('doctors_portal').collection('services');
         const bookingCollection = client.db('doctors_portal').collection('bookings');
         const userCollection = client.db('doctors_portal').collection('users');
+        const doctorCollection = client.db('doctors_portal').collection('doctors');
 
         app.get('/service', async (req, res) => {
             const query = {};
@@ -109,7 +110,7 @@ async function run() {
                 return res.status(403).send({ message: 'forbidden access' })
             }
 
-        })
+        });
 
         app.post('/booking', async (req, res) => {
             const booking = req.body;
@@ -120,7 +121,13 @@ async function run() {
             }
             const result = await bookingCollection.insertOne(booking);
             return res.send({ success: true, result })
-        })
+        });
+
+        app.post('/doctor', async (req, res) => {
+            const doctor = req.body;
+            const result = await doctorCollection.insertOne(doctor);
+            res.send(result);
+        });
 
     }
     finally {
